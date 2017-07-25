737c57f805076d8ec5026fed681f81602aa68d63
java <bmecuriaw@gmail.com>
2017-07-25 04:48:10 -0700

    first commit

--- src/firstproject/OddAndEven.java
+++ src/firstproject/OddAndEven.java
@@ -1,10 +1,12 @@
+
 package firstproject;
 
 public class OddAndEven {
 	
 	public static void main(String args[]){
-		
+		 
 		for(int i=1;i<=100;i++){
+			// This will check if the number is odd 
 			if(i%2==1)
 				System.out.println(i+ "");
 			else 
