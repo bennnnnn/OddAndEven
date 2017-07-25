5f052ec766be955563a7bcb014b8efb3d01f72b9
java <bmecuriaw@gmail.com>
2017-07-24 21:27:33 -0700

    commiting

---
+++ src/firstproject/OddAndEven.java
@@ -0,0 +1,15 @@
+package firstproject;
+
+public class OddAndEven {
+	
+	public static void main(String args[]){
+		
+		for(int i=1;i<=100;i++){
+			if(i%2==1)
+				System.out.println(i+ "");
+			else 
+				System.out.println(i + " is an even number");
+		}
+	}
+	
+}
